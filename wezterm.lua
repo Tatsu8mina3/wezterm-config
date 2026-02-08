9ac54522aa3c34a4bc230cf7ca603286a7bf318a
TATSUYA MINAMI <tatsu8mina3@tatsuyanoMac-mini.local>
2026-02-08 17:23:57 +0900

    auto-sync via Claude Code

--- wezterm.lua
+++ wezterm.lua
@@ -13,7 +13,7 @@ wezterm.on("gui-startup", function()
 end)
 config.font_size = 12.0
 config.use_ime = true
-config.window_background_opacity = 0.6
+config.window_background_opacity = 0.1
 config.macos_window_background_blur = 20
 
 ----------------------------------------------------
